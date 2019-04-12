1830b788db20504a1667c01161cd737ad9f144d1
ararararar Money <bl.wolenetz@gmail.com>
2019-04-12 11:01:09 -0700

    collider start

---
+++ Project/Source/Collider.h
@@ -0,0 +1,100 @@
+//------------------------------------------------------------------------------
+//
+// File Name:	Collider.h
+// Author(s):	Jeremy Kings (j.kings)
+// Project:		BetaFramework
+// Course:		WANIC VGP2
+//
+// Copyright © 2018 DigiPen (USA) Corporation.
+//
+//------------------------------------------------------------------------------
+
+#pragma once
+
+//------------------------------------------------------------------------------
+// Include Files:
+//------------------------------------------------------------------------------
+
+#include "Component.h"
+
+//------------------------------------------------------------------------------
+
+//------------------------------------------------------------------------------
+// Forward Declarations:
+//------------------------------------------------------------------------------
+
+class Transform;
+class Physics;
+class GameObject;
+typedef void(*CollisionEventHandler)(GameObject& callingObject, GameObject& otherObject);
+
+//------------------------------------------------------------------------------
+// Public Consts:
+//------------------------------------------------------------------------------
+
+typedef enum ColliderType
+{
+	ColliderTypePoint,
+	ColliderTypeCircle,
+	ColliderTypeRectangle
+} ColliderType;
+
+//------------------------------------------------------------------------------
+// Public Structures:
+//------------------------------------------------------------------------------
+
+// Collider class - Detects collisions between objects
+class Collider : public Component
+{
+public:
+	//------------------------------------------------------------------------------
+	// Public Functions:
+	//------------------------------------------------------------------------------
+
+	// Allocate a new collider component.
+	// Params:
+	//   type = The type of collider (see the ColliderType enum).
+	Collider(ColliderType type);
+
+	// Set component pointers.
+	void Initialize() override;
+
+	// Draw collision shape
+	virtual void Draw() = 0;
+
+	// Check if two objects are colliding and send collision events.
+	// Params:
+	//	 other = Reference to the second collider component.
+	void CheckCollision(const Collider& other);
+
+	// Perform intersection test between two arbitrary colliders.
+	// Params:
+	//	 other = Reference to the second collider component.
+	virtual bool IsCollidingWith(const Collider& other) const = 0;
+
+	// Get the type of this component.
+	ColliderType GetType() const;
+
+	// Sets the collision handler function for the collider.
+	// Params:
+	//   handler = A pointer to the collision handler function.
+	void SetCollisionHandler(CollisionEventHandler handler);
+
+protected:
+	// Component pointers
+	Transform* transform;
+	Physics* physics;
+
+private:
+	//------------------------------------------------------------------------------
+	// Private Variables:
+	//------------------------------------------------------------------------------
+
+	// The type of collider used by this component.
+	ColliderType type;
+
+	// Function pointer for collision handler
+	CollisionEventHandler handler;
+};
+
+//------------------------------------------------------------------------------
---
+++ Project/Source/Intersection2D.cpp
@@ -0,0 +1,96 @@
+//------------------------------------------------------------------------------
+// Include Files:
+//------------------------------------------------------------------------------
+
+#include "Intersection2D.h"
+
+//------------------------------------------------------------------------------
+// Public Functions:
+//------------------------------------------------------------------------------
+
+// Check whether a point and circle intersect.
+// Params:
+//  point = The point in the intersection.
+//  circle = The circle in the intersection.
+// Returns:
+//   True if intersection, false otherwise.
+bool PointCircleIntersection(const Vector2D& point, const Circle& circle) {
+	if (point.Distance(circle.center) <= circle.radius) {
+		return true;
+	}
+	return false;
+}
+
+// Check whether a point and rectangle intersect.
+// Params:
+//  point = The point in the intersection.
+//  rect  = The rectangle in the intersection.
+// Returns:
+//   True if intersection, false otherwise.
+bool PointRectangleIntersection(const Vector2D& point, const BoundingRectangle& rect) {
+	if (point.x < rect.top && point.x > rect.bottom && point.y > rect.left && point.y < rect.right) {
+		return true;
+	}
+	return false;
+}
+
+// Check whether two circles intersect.
+// Params:
+//  circle1 = The first circle.
+//  circle2 = The second circle.
+// Returns:
+//   True if intersection, false otherwise.
+bool CircleCircleIntersection(const Circle& circle1, const Circle& circle2) {
+	if (circle1.center.Distance(circle2.center) >= circle1.radius + circle2.radius) {
+		return true;
+	}
+	return false;
+}
+
+// Check whether two rectangles intersect.
+// Params:
+//  rect1 = The first rectangle.
+//	rect2 = The second rectangle.
+// Returns:
+//   True if intersection, false otherwise.
+bool RectangleRectangleIntersection(const BoundingRectangle& rect1, const BoundingRectangle& rect2) {
+	if (rect1.left < rect2.right &&
+		rect1.right > rect2.left &&
+		rect1.bottom < rect2.top &&
+		rect1.top > rect2.bottom) {
+		return true;
+	}
+	return false;
+}
+
+// Check whether a rectangle and circle intersect.
+// Params:
+//  rect = The rectangle.
+//  circle  = The circle.
+// Returns:
+//   True if intersection, false otherwise.
+bool RectangleCircleIntersection(const BoundingRectangle& rect, const Circle& circle) {
+
+	Vector2D point = circle.center;
+
+	if (point.x > rect.right) {
+		point.x = rect.right;
+	} else if (point.x < rect.left) {
+		point.x = rect.left;
+	}
+
+	if (point.y > rect.top) {
+		point.y = rect.top;
+	} else if (point.y < rect.bottom) {
+		point.y = rect.bottom;
+	}
+
+	if (point.Distance(circle.center) <= circle.radius) {
+		return true;
+	}
+	
+	return false;
+
+}
+
+//------------------------------------------------------------------------------
